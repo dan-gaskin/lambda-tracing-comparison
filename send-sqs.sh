d9648841307c966342c15172a282c979f878e088
Dan <daniel.gaskin@contino.io>
2020-07-04 15:04:09 +1000

    modified script and readme

--- send-sqs.sh
+++ send-sqs.sh
@@ -1,9 +1,17 @@
 #! /bin/bash
-SQS_LUMIGO=$(aws cloudformation describe-stack-resources --stack-name lambda-tracing-comparison-dev --query 'StackResources[?LogicalResourceId==`SQSXrayTrigger`].PhysicalResourceId' --output text)
-SQS_XRAY=$(aws cloudformation describe-stack-resources --stack-name lambda-tracing-comparison-dev --query 'StackResources[?LogicalResourceId==`SQSLumigoTrigger`].PhysicalResourceId' --output text)
+STACK_NAME=$(sls info | grep stack | sed 's/[^ ]* //')
+printf "stack name resolved as ${STACK_NAME}\n"
 
-for ((i=1;i<=100;i++)); 
+SQS_XRAY=$(aws cloudformation describe-stack-resources --stack-name ${STACK_NAME} --query 'StackResources[?LogicalResourceId==`SQSXrayTrigger`].PhysicalResourceId' --output text)
+printf "xray sqs arn resolved as ${SQS_XRAY}\n"
+
+SQS_LUMIGO=$(aws cloudformation describe-stack-resources --stack-name ${STACK_NAME} --query 'StackResources[?LogicalResourceId==`SQSLumigoTrigger`].PhysicalResourceId' --output text)
+printf "lumigo sqs arn resolved as ${SQS_LUMIGO}\n"
+
+for ((i=1;i<=$1;i++)); 
 do 
+    printf "\nsending message ${i} to sqs triggers\n"
     aws sqs send-message --queue-url ${SQS_LUMIGO} --message-body hi
     aws sqs send-message --queue-url ${SQS_XRAY} --message-body hi
+    printf "\n"
 done
